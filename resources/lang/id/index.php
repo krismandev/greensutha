bb24c52b1db96ee586ded9ced87196155fdf62dc
krismandev <krismanpratama@gmail.com>
2020-10-10 17:23:37 +0700

    bahasa

---
+++ resources/lang/id/index.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  'kampus_hijau' => 'Kampus Hijau',
+  'event' => 'Events',
+  'tim' => 'Tim Pengelola',
+  'berita_terbaru' => 'Berita terbaru',
+  'foto' => 'Foto',
+  'poster' => 'Poster',
+  'lebih_dekat' => 'Lebih dekat dengan kami',
+  'kontak' => 'Kontak',
+  'menu_cepat' => 'Menu Cepat',
+
+  'baca_selengkapnya' => 'Baca selengkapnya...',
+  'selayang_pandang' => 'Selayang Pandang',
+];
